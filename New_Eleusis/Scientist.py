cc704bffc6d8efdd8229e015cfd82c28e077ac52
adudi1 <adudi1@umbc.edu>
2016-11-18 23:53:27 -0500

    stub functions

---
+++ New_Eleusis/Scientist.py
@@ -0,0 +1,34 @@
+
+class State:
+
+    def __init__(self, state):
+        self.state = state
+
+    def setRule(self, rule):
+        self.rule = rule
+
+    def rule(self):
+        return self.rule
+
+    def boardState(self):
+        return self.state
+
+    def play(self,card):
+        #to do
+        pass
+
+    def scientist(self):
+        pass
+
+    def score(self):
+        pass
+
+
+def hypothesis():
+    #return hypotesis to be tested
+    pass
+
+def card():
+    #return card to be played to test hypothesis
+    pass
+
